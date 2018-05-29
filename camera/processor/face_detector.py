94689a98dc730b617ac4bd39db8918bfa7aa8c6f
dmiyamoto <m2985.damiyamoto@gmail.com>
2018-05-29 20:35:44 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -11,6 +11,10 @@ class FaceDetector(object):
         self.flip = flip
         time.sleep(2.0)
 
+        self.argvs = sys.argv
+        self.face_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_frontalface_default.xml')
+        if len(self.argvs) >= 2 and "eyes" in self.argvs:
+            self.eye_cascade = cv2.CascadeClassifier('camera/processor/model/haarcascades/haarcascade_eye.xml')
         # opencvの顔分類器(CascadeClassifier)をインスタンス化する
 
     def __del__(self):
@@ -28,6 +32,16 @@ class FaceDetector(object):
         return jpeg.tobytes()
 
     def process_image(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 3)
+        for (x,y,w,h) in faces:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+            if len(self.argvs) >= 2 and "eyes" in self.argvs:
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = self.eye_cascade.detectMultiScale(roi_gray)
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
         # opencvでframe(カラー画像)をグレースケールに変換
 
         # 上記でグレースケールに変換したものをインスタンス化した顔分類器の
